4ac060390ecfcb339d8d6f675a81fce8a53f80b0
atabarezunal <atabarez@unal.edu.co>
2026-04-03 00:10:44 -0500

    Feat: Skill creation and user assignment

--- user_service/users/urls.py
+++ user_service/users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import get_profile, create_profile
+from .views import get_profile, create_profile, create_skill, get_skills, assign_skill
 
 urlpatterns = [
     path('profile/<int:user_id>/', get_profile),
     path('profile/', create_profile),
+    path('skills/', get_skills),
+    path('skills/create/', create_skill),
+    path('skills/assign/', assign_skill),
 ]
--- user_service/users/views.py
+++ user_service/users/views.py
@@ -2,6 +2,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
+from .models import Skill, UserSkill, Profile
+from .serializers import SkillSerializer
 
 @api_view(['GET'])
 def get_profile(request, user_id):
@@ -18,5 +20,41 @@ def create_profile(request):
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data)
-
     return Response(serializer.errors, status=400)
+
+
+@api_view(['POST'])
+def create_skill(request):
+    serializer = SkillSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
+
+
+@api_view(['GET'])
+def get_skills(request):
+    skills = Skill.objects.all()
+    return Response(SkillSerializer(skills, many=True).data)
+
+
+
+@api_view(['POST'])
+def assign_skill(request):
+    profile_id = request.data.get('profile_id')
+    skill_id = request.data.get('skill_id')
+    experience = request.data.get('experience')
+    try:
+        profile = Profile.objects.get(profile_id=profile_id)
+        skill = Skill.objects.get(id_skill=skill_id)
+        user_skill, created = UserSkill.objects.get_or_create(
+            profile=profile,
+            skill=skill,
+            defaults={'experience': experience}
+        )
+        if not created:
+            user_skill.experience = experience
+            user_skill.save()
+        return Response({"message": "Skill asignada correctamente"})
+    except Exception as e:
+        return Response({"error": str(e)}, status=400)
