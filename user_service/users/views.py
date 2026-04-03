81db1b9d4244fcb303c50c04f5f6bf093575b63a
atabarezunal <atabarez@unal.edu.co>
2026-04-02 21:36:48 -0500

    Feat and refactor: add the views and routes, modifying the settings for middleware management

--- user_service/users/views.py
+++ user_service/users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Profile
+from .serializers import ProfileSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def get_profile(request, user_id):
+    profile = Profile.objects.filter(user_id=user_id).first()
+    if not profile:
+        return Response({'error': 'Not found'}, status=404)
+
+    return Response(ProfileSerializer(profile).data)
+
+
+@api_view(['POST'])
+def create_profile(request):
+    serializer = ProfileSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+
+    return Response(serializer.errors, status=400)
